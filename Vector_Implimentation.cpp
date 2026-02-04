a51c23cc094e432cf095fc3726f42a675f3df25b
qburst-bharadwaj <bharadwaj@qburst.com>
2026-02-05 00:29:37 +0530

    Vector implimentation

---
+++ Vector_Implimentation.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+static int counter = 0;
+
+template <class type>
+class vector
+{
+private:
+	int length;
+	int capacity;
+	type *ptr;
+
+public:
+	vector()
+	{
+		length = 1;
+		capacity = 1;
+		ptr = new type[capacity];
+	}
+
+	void insert(type data)
+	{
+		if (counter != 0)
+		{
+			if (length == capacity)
+			{
+				capacity = 2 * capacity;
+			}
+		}
+
+		*(ptr + counter) = data;
+		counter = counter + 1;
+	}
+
+	void showData()
+	{
+		for (int i = 0; i < counter; i++)
+		{
+			cout << *(ptr + i) << endl;
+		}
+	}
+};
+
+int main()
+{
+	vector<int> v1;
+	v1.insert(10);
+	v1.insert(57);
+	v1.showData();
+	return 0;
+}
