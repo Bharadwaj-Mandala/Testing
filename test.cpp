52cf4431b874f39763a6174a60b57bb2b516a651
qburst-bharadwaj <bharadwaj@qburst.com>
2026-02-04 14:41:57 +0530

    testing git

---
+++ test.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Testing
+{
+public:
+	void operator()(int x)
+	{
+		if (x % 2 == 0)
+		{
+			cout << "Even" << x << endl;
+		}
+		else
+		{
+			cout << "Odd" << x << endl;
+		}
+	}
+};
+
+void f1()
+{
+	int a = 10;
+	int *p = NULL;
+
+	*p = 20;
+	cout << *p;
+}
+
+int main()
+{
+	new int;
+
+	return 0;
+}
